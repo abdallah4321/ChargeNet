ce37002c6e2aa52c7a99100a66ee285ed705c60e
abdallah ramadan <abdallahramadan222@gmail.com>
2025-09-23 16:25:45 +0300

    feat(models): :sparkles: add DB models

---
+++ src/models/UnitsModels.js
@@ -0,0 +1,23 @@
+// models/Units.js
+import mongoose from 'mongoose';
+
+const unitsSchema = new mongoose.Schema(
+  {
+    StationsId: { type: mongoose.Schema.Types.ObjectId, ref: 'Stations' },
+    // vehiclesId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicles' },
+    name: { type: String, required: true },
+    unitType: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ['available', 'in-use', 'maintenance'],
+      default: 'available',
+    },
+    pricePerHour: { type: Number, required: true },
+    lastMaintenance: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+const Units = mongoose.model('Units', unitsSchema);
+
+export default Units;
